1c9efe04305136a6ceb165d986298ebee8fb9424
heramb007 <50517960+heramb007@users.noreply.github.com>
2023-09-04 20:23:28 +0530

    Update server.js

--- cx/slack/server.js
+++ cx/slack/server.js
@@ -5,12 +5,12 @@
  */
 const slackSigningSecret = '53696b317b548e68817474c6c8da6ace';
 const slackToken = 'xoxb-5848185848612-5831219072359-13bDX9rkrUHSL84d9nTwKbYo';
-//const projectId = 'uber-poc-397822';
-//const locationId = 'global';
-//const agentId = '95c6a58e-3d5f-4fd1-8ab6-09cf8b2b7828';
-//const languageCode = 'en';
-//const port = process.env.PORT || 8080;
-//const hostname = '0.0.0.0';
+const projectId = 'uber-poc-397822';
+const locationId = 'global';
+const agentId = '95c6a58e-3d5f-4fd1-8ab6-09cf8b2b7828';
+const languageCode = 'en';
+const port = process.env.PORT || 8080;
+const hostname = '0.0.0.0';
 const structProtoToJson =
     require('../../botlib/proto_to_json.js').structProtoToJson;
 
